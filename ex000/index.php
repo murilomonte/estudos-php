bc2d665c795d132468b6c06ab8d0b05775fa1a19
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2023-03-21 10:09:23 -0300

    exercícios iniciais

---
+++ ex000/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ptbr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <?php 
+        echo "Olá, mundo";
+    ?>
+</body>
+</html>
---
+++ ex002/index.php
@@ -0,0 +1,8 @@
+<?php 
+    // seta a timezone
+    date_default_timezone_set("America/Sao_Paulo");
+
+    // o pontinho serve para concatenar duas strings
+    echo "hoje é dia " . date("d/M/Y");
+    echo " e a hora atual é " . date("G:i:s");
+?>
