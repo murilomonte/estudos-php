39dfff3968edba0a336356b100068173fdd66e62
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2023-04-06 14:24:24 -0300

    formulários retroalimentados

---
+++ estudos/CEV/exercicios/ex006/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício php</title>
+    <link rel="stylesheet" href="../../csstemp/style.css">
+</head>
+<body>
+    <?php 
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0;
+    ?>
+    <main>
+        <h1>Somador de valores</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="v1">Valor 1</label>
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+
+            <input type="submit" value="Somar">
+        </form>
+    </main>
+
+    <section>
+        <h2>Resultado da soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            echo "<p>A soma entre os valores $valor1 e $valor2 é igual a $soma</p>"
+        ?>
+    </section>
+</body>
+</html>
